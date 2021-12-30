a2c5aea9e7fef273d3dc1f9d582d746cc58dbdaa
tinaismail <ismait1@mcmaster.ca>
2021-12-30 01:28:46 -0500

    set up environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: 
+  `
+  <app-home></app-home>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
